c4d60a97569d6c2c668768d26cfada4cd70a708a
SMEETT <tbr@tutamail.com>
2022-01-15 12:40:53 +0100

    deleted new CT2 [deploy]

--- src/api/new-ct-2/controllers/new-ct-2.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  new-ct-2 controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::new-ct-2.new-ct-2');
