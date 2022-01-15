4323b6c3f65fede57b6b7b4a8997a9364557ca7d
SMEETT <tbr@tutamail.com>
2022-01-15 11:37:19 +0100

    more test-collection-types / changed DB config again [deploy]

---
+++ src/api/thirdct/controllers/thirdct.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  thirdct controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::thirdct.thirdct');
---
+++ src/api/thirdct/services/thirdct.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * thirdct service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::thirdct.thirdct');
