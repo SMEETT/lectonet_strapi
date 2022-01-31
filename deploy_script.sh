a07bb9b8a26b7dee651ad5fe066a093a4012d170
SMEETT <tbr@tutamail.com>
2022-01-31 20:32:52 +0100

    redeploying existing data [deploy]

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,9 +1,9 @@
 pm2 stop lectonet_strapi
 cd /var/lib/pm2node/lectonet_strapi
 npm install
-npm run build
-git clean -f
-git clean -d -f
+# npm run build
+# git clean -f
+# git clean -d -f
 git checkout main
 pm2 delete lectonet_strapi
 pm2 restart ecosystem.config.js
