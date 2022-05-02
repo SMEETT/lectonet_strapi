9fe7ec853ffc11c573716b81660befc8496fb2ba
Boris Fries <tbr@tutamail.com>
2022-05-02 13:34:33 +0200

    changed deploy-script [deploy]

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,10 +1,11 @@
-pm2 stop lectonet_strapi
+pm2 kill
+cd /var/lib/pm2node/lectonet_frontend
+pm2 start ecosystem.config.js
 cd /var/lib/pm2node/lectonet_strapi
 npm install
 npm run build
 # git clean -f
 # git clean -d -f
 # git checkout main
-pm2 delete lectonet_strapi
-pm2 restart ecosystem.config.js
+pm2 start ecosystem.config.js
 pm2 save
